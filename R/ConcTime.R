f85448b2857cb9dee38ca8d0b32eec0085f09247
Sungpil Han <shan@acp.kr>
2017-08-14 08:08:01 +0000

    version 0.2.0

---
+++ R/ConcTime.R
@@ -0,0 +1,74 @@
+#' Create a dataset of the concentration-time curve of single oral administration of caffeine
+#'
+#' \code{caffConcTime} will create a dataset of the concentration-time curve
+#' 
+#' @param Weight Body weight (kg)
+#' @param Dose Dose of single caffeine (mg)
+#' @param N The number of simulated subjects
+#' @return The dataset of concentration and time of simulated subjects
+#' @export
+#' @examples 
+#' caffConcTime(Weight = 20, Dose = 200, N = 20)
+#' caffConcTime(20, 200)
+#' @import dplyr
+#' @seealso \url{https://asancpt.github.io/caffsim}
+
+caffConcTime <- function(Weight, Dose, N = 20){
+  ggConc <- caffDataset(Weight, Dose, N) %>% 
+    select(CL, V, Ka, Ke) %>% 
+    mutate(Subject = row_number()) %>% 
+    left_join(expand.grid(
+      x = seq(1, N, length.out = N),  #Subjecti
+      y = seq(0,24, by = 0.1)) %>% # Time
+        select(Subject=x, Time=y), by = "Subject") %>% 
+    mutate(Conc = Dose / V * Ka / (Ka - Ke) * (exp(-Ke * Time) - exp(-Ka * Time))) %>% 
+    select(Subject, Time, Conc)
+  return(ggConc)
+}
+
+
+#' Create a dataset of the concentration-time curve of multiple dosing of caffeine
+#'
+#' \code{caffConcTimeMulti} will create a dataset of the concentration-time curve of multiple oral administrations of caffeine
+#' 
+#' @param Weight Body weight (kg)
+#' @param Dose Dose of single caffeine (mg)
+#' @param N The number of simulated subjects
+#' @param Tau The interval of multiple dosing (hour)
+#' @param Repeat The number of dosing
+#' @return The dataset of concentration and time of simulated subjects of multiple dosing
+#' @export
+#' @examples 
+#' caffConcTimeMulti(Weight = 20, Dose = 200, N = 20, Tau = 8, Repeat = 4)
+#' caffConcTimeMulti(20, 200)
+#' @import dplyr
+#' @seealso \url{https://asancpt.github.io/caffsim}
+
+caffConcTimeMulti <- function(Weight, Dose, N = 20, Tau = 8, Repeat = 4){
+  Subject <- seq(1, N, length.out = N) # 
+  Time <- seq(0, 96, length.out = 481) # 
+  Grid <- expand.grid(x = Subject, y = Time) %>% select(Subject=x, Time=y)
+  
+  ggsuper <- caffDataset(Weight, Dose, N) %>% 
+    select(CL, V, Ka, Ke) %>% 
+    mutate(Subject = row_number()) %>% 
+    left_join(Grid, by = "Subject") %>% 
+    mutate(Conc = Dose / V * Ka / (Ka - Ke) * (exp(-Ke * Time) - exp(-Ka * Time))) %>% 
+    group_by(Subject) %>% 
+    mutate(ConcOrig = Conc, 
+           ConcTemp = 0)
+  
+  ## Superposition
+  for (i in 1:Repeat){
+    Frame <- Tau * 5 * i
+    ggsuper <- ggsuper %>% 
+      mutate(Conc = Conc + ConcTemp) %>% 
+      mutate(ConcTemp = lag(ConcOrig, n = Frame, default = 0))
+  }
+  
+  ggsuper <- ggsuper %>% select(Subject, Time, Conc)
+  return(ggsuper)
+}
+
+globVar <- utils::globalVariables(c('CL', 'V', 'Ka', 'Ke', 'x', 'y', 'Time', 'Subject', 'Conc', 'AI', 'AUC', 'Aavss', 'Cavss', 'Cmax', 'Cmaxss', 'Cminss', 'ConcOrig', 'ConcTemp', 'Half_life', 'Tmax', 'X1', 'X2', 'X3', 'eta1', 'eta2', 'eta3'))
+
---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Unit data of PK parameters
+#'
+#' A dataset containing information regarding unit data of pharmacokinetic parameters
+#'
+#' @format A data frame with 16 rows and 2 variables:
+#' \describe{
+#'   \item{Parameters}{Abbreviated pharmacokinetic parameters}
+#'   \item{Parameter}{Pharmacokinetic parameters in full name}
+#' }
+#' @seealso \url{https://asancpt.github.io/caffsim}
+#' @export
+
+"UnitTable"
